dc40fafcd3f786940a71e43bc96e429ea94a16b0
Brett Profitt <brett.profitt@gmail.com>
2011-08-08 11:21:39 -0700

    Fixed link for trac.

--- views/default/canvas/layouts/front_page.php
+++ views/default/canvas/layouts/front_page.php
@@ -60,7 +60,7 @@
 	<h2>More</h2>
 	<p>Contribute to Elgg. Open source works best as a community&hellip;</p>
 	<ul>
-		<li class="bug_tracker"><b>Bug tracker</b><br /><a href="http://trac.elgg.org/elgg/">Core engine</a>.</li>
+		<li class="bug_tracker"><b>Bug tracker</b><br /><a href="http://trac.elgg.org/">http://trac.elgg.org</a></li>
 		<li class="mailing_list"><b>Mailing list and IRC</b><br /><a href="http://groups.google.com/group/elgg-development/">For developers</a> also <a href="http://webchat.freenode.net/?channels=#elgg">#Elgg on Freenode.net.</a></li>
 		<li class="api_reference"><b>API reference</b><br />The best way to find out what functions exist. <a href="http://reference.elgg.org">Check it out</a>.</li>
 	</ul>
