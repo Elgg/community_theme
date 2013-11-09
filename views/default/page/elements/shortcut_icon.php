93f62132b82bff0b9355dc1e10882e8f39eb65a2
Paweł Sroka <srokap@gmail.com>
2013-11-09 18:57:29 +0100

    Corrections of code deprecated since Elgg 1.8

--- views/default/page/elements/shortcut_icon.php
+++ views/default/page/elements/shortcut_icon.php
@@ -3,4 +3,4 @@
  * Elgg favicon
  */
 ?>
-<link rel="SHORTCUT ICON" href="<?php echo $vars['url']; ?>mod/community_theme/graphics/favicon.ico" />
+<link rel="SHORTCUT ICON" href="<?php echo elgg_get_site_url(); ?>mod/community_theme/graphics/favicon.ico" />
