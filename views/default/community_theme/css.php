8ef3c8a5df8c0eef7cd3747430633aae5cb9d67f
Per Jensen <admin@perjensen-online.dk>
2014-06-02 18:04:56 +0200

    Adds responsive CSS to index page
    Fixes #14

--- views/default/community_theme/css.php
+++ views/default/community_theme/css.php
@@ -234,10 +234,20 @@ body {
 		max-width: 100%;
         width: 100%;
     }
+	.elgg-module-community {
+		margin: 0 0 20px 0;
+	}
     .elgg-module-register {
         width: 100%;
 		margin-top: 20px;
     }
+	#top_left {
+		margin: 0 0 20px 0;
+	}
+	.elgg-col-1of4,
+	.elgg-col-3of4 {
+		width: 100%;
+	}
 	.elgg-menu-footer-meta,
 	.elgg-menu-footer-default {
 		padding: 0 20px;
