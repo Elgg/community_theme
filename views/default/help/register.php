545f816d5958f3d188ad310bb6c5304ba817ba34
Per Jensen <admin@perjensen-online.dk>
2014-06-08 13:05:50 +0200

    Updates link to terms on register page

--- views/default/help/register.php
+++ views/default/help/register.php
@@ -12,7 +12,7 @@ $body =
 		<li>Use the <a href="http://demo.elgg.org">Demo Elgg Site</a> to test Elgg. (Test content on the community site will be deleted.)</li>
 		<li>Be courteous, friendly, and patient with all members of the site.</li>
 		<li>Be respectful of plugin developers\' time and priorities.</li>
-		<li>Read and follow the <a href="http://community.elgg.org/pg/expages/read/Terms/">Terms and Conditions.</a></li>
+		<li>Read and follow the <a href="http://community.elgg.org/terms">Terms and Conditions.</a></li>
 	</ul>';
 
 ?>
