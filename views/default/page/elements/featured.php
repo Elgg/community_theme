268bae854adb683f98a5db97615b27903ee75cdb
Per Jensen <admin@perjensen-online.dk>
2014-06-09 10:55:55 +0200

    Adds missing file for image module
    Referencing #29

---
+++ views/default/page/elements/featured.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Front page featured module
+ */
+?>
+
+<div class="elgg-featured-container">
+	<div class="elgg-featured">
+		<div class="featured-image">
+			<div class="elgg-caption">
+				<div class="elgg-inner">
+					<h1><span>elgg</span>Community</h1>
+					<h2><?php echo elgg_echo('community_theme:description'); ?></h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
