f6818321309f613496185f801cb546aeae8305ea
Per Jensen <admin@perjensen-online.dk>
2014-06-28 07:10:00 +0200

    Updates the use of elgg_view_layout
    Fixes #36

--- index.php
+++ index.php
@@ -7,5 +7,5 @@
 elgg_push_context('community');
 $class = array('class' => 'community-landing-page');
 
-$body = elgg_view_layout('community_landing_page', $params);
+$body = elgg_view_layout('community_landing_page');
 echo elgg_view_page(null, $body, 'default', array('body_attrs' => $class));
