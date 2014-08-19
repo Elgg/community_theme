75a79abd1299cf49bdfc7e204f8a95e64b2d9e03
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2014-08-19 09:31:32 +0300

    Fixes count of featured showcases and some whitespace issues

--- views/default/community_theme/modules/showcase.php
+++ views/default/community_theme/modules/showcase.php
@@ -5,13 +5,16 @@
  */
 
 $title = elgg_echo('community_theme:title:showcase');
- 
+
 // get number of featured sites
 $options = array(
-    'type' => 'object',
-    'subtype' => 'showcase',
-    'metadata_name_value_pairs' => array('name' => 'featured', 'value' => 1),
-    'count' => true
+	'type' => 'object',
+	'subtype' => 'showcase',
+	'metadata_name_value_pairs' => array(
+		'name' => 'showcase_featured',
+		'value' => 1
+	),
+	'count' => true
 );
 $num_featured = elgg_get_entities_from_metadata($options);
 
@@ -19,8 +22,8 @@ $content = elgg_echo('community_theme:showcase', array($num_featured));
 
 $content .= elgg_view('output/url', array(
 	'class' => 'elgg-button',
-	'href' => '/showcase', 
-	'text' => elgg_echo('community_theme:showcase:link'), 
+	'href' => '/showcase',
+	'text' => elgg_echo('community_theme:showcase:link'),
 ));
 
 echo elgg_view_module('community', $title, $content);
