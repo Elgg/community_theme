9adddf599eb21c3c24dbe17478581c4610cb167d
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2014-08-21 16:26:31 +0300

    Help module link now takes the user to "/discussion/all" instead of "/discussion"
    Two benefits:
     - The site menu item is active
     - This page has the "Add discussion topic" button

--- views/default/community_theme/modules/help.php
+++ views/default/community_theme/modules/help.php
@@ -12,8 +12,8 @@ $content = elgg_echo('community_theme:help', array($num_members));
 
 $content .= elgg_view('output/url', array(
 	'class' => 'elgg-button',
-	'href' => '/discussion', 
-	'text' => elgg_echo('community_theme:help:link'), 
+	'href' => '/discussion/all',
+	'text' => elgg_echo('community_theme:help:link'),
 ));
 
 echo elgg_view_module('community', $title, $content);
